3ab6bad2f5731f1131f9b47497d6f07879328a77
Shu Sam Chen <sschen@microsoft.com>
2015-11-10 14:51:08 +0000

    Additional tests for AssemblyResolver.

--- Tests/CommonTests/AssemblyResolverTests.cs
+++ Tests/CommonTests/AssemblyResolverTests.cs
@@ -1,13 +1,11 @@
 using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ExampleAssembly;
-using System.CodeDom.Compiler;
 using Roslyn.SonarQube.AnalyzerPlugins;
 using System.IO;
 using Tests.Common;
 using System.Collections.Generic;
 using System.Reflection;
-using Microsoft.CSharp;
 
 namespace CommonTests
 {
@@ -20,7 +18,7 @@ namespace CommonTests
         /// Tests the loading of an assembly with a single type and no dependencies. This should succeed even without AssemblyResolver.
         /// </summary>
         [TestMethod]
-        public void TestSimpleAssemblyLoad()
+        public void TestGeneralAssemblyLoad()
         {
             // Arrange
             TestLogger logger = new TestLogger();
@@ -53,6 +51,56 @@ namespace CommonTests
             // Assert
             Assert.IsNotNull(simpleProgram);
             Assert.AreEqual<Type>(simpleProgramType, typeof(SimpleProgram));
+        }
+
+        /// <summary>
+        /// Tests the case where assembly resolution should fail correctly.
+        /// </summary>
+        [TestMethod]
+        public void TestAssemblyResolutionFail()
+        {
+            // Arrange
+            TestLogger logger = new TestLogger();
+            string testFolder = TestUtils.CreateTestDirectory(this.TestContext);
+
+            // Act
+            Assembly resolveResult;
+            using (AssemblyResolver assemblyResolver = new AssemblyResolver(testFolder, logger))
+            {
+                ResolveEventArgs resolveEventArgs = new ResolveEventArgs("nonexistent library", this.GetType().Assembly);
+                resolveResult = assemblyResolver.CurrentDomain_AssemblyResolve(this, resolveEventArgs);
+            }
+
+            // Assert
+            Assert.IsNull(resolveResult);
+        }
+
+        /// <summary>
+        /// Tests the case where assembly resolution should succeed.
+        /// </summary>
+        [TestMethod]
+        public void TestAssemblyResolution()
+        {
+            // Arrange
+            TestLogger logger = new TestLogger();
+            string testFolder = TestUtils.CreateTestDirectory(this.TestContext);
+
+            Assembly sourceAssembly = typeof(SimpleProgram).Assembly;
+            string sourceAssemblyPath = sourceAssembly.Location;
+            string destinationAssemblyPath = Path.Combine(testFolder, Path.GetFileName(sourceAssemblyPath));
+            File.Copy(sourceAssemblyPath, destinationAssemblyPath);
+
+            // Act
+            Assembly resolveResult;
+            using (AssemblyResolver assemblyResolver = new AssemblyResolver(testFolder, logger))
+            {
+                ResolveEventArgs resolveEventArgs = new ResolveEventArgs(sourceAssembly.FullName, this.GetType().Assembly);
+                resolveResult = assemblyResolver.CurrentDomain_AssemblyResolve(this, resolveEventArgs);
+            }
+
+            // Assert
+            Assert.IsNotNull(resolveResult);
+            Assert.AreEqual<string>(sourceAssembly.ToString(), resolveResult.ToString());
         }
     }
 }
