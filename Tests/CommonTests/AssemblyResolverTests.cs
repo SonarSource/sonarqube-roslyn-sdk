014251a1ada7643962b68fa7e15242c54fcfb7c8
Shu Sam Chen <sschen@microsoft.com>
2015-12-11 10:05:11 +0000

    Added missing header to AssemblyResolverTests.

--- Tests/CommonTests/AssemblyResolverTests.cs
+++ Tests/CommonTests/AssemblyResolverTests.cs
@@ -1,3 +1,9 @@
+//-----------------------------------------------------------------------
+// <copyright file="AssemblyResolverTests.cs" company="SonarSource SA and Microsoft Corporation">
+//   Copyright (c) SonarSource SA and Microsoft Corporation.  All rights reserved.
+//   Licensed under the MIT License. See License.txt in the project root for license information.
+// </copyright>
+//-----------------------------------------------------------------------
 using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.IO;
