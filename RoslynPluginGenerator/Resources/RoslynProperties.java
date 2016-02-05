82b8c520c0cbb091481e8323f7f36f346c07c1e2
duncanp <duncanp@microsoft.com>
2016-02-05 12:50:35 +0000

    [SFSRAP-17] Added omitted file changes

--- RoslynPluginGenerator/Resources/RoslynProperties.java
+++ RoslynPluginGenerator/Resources/RoslynProperties.java
@@ -5,22 +5,22 @@ import org.sonar.api.config.PropertyDefinition;
 // Defines the metadata properties required by the C# plugin
 public final class RoslynProperties {
 
-  public static PropertyDefinition AnalyzerId = PropertyDefinition.builder(PluginRulesDefinition.KEY + ".analyzerId")
+  public static PropertyDefinition AnalyzerId = PropertyDefinition.builder(PluginRulesDefinition.REPOSITORY_ID + ".analyzerId")
 	.defaultValue("[ROSLYN_ANALYZER_ID]")
 	.hidden()
 	.build();
 
-  public static PropertyDefinition RuleNamespace = PropertyDefinition.builder(PluginRulesDefinition.KEY + ".ruleNamespace")
+  public static PropertyDefinition RuleNamespace = PropertyDefinition.builder(PluginRulesDefinition.REPOSITORY_ID + ".ruleNamespace")
 	.defaultValue("[ROSLYN_RULE_NAMESPACE]")
 	.hidden()
 	.build();
 
-  public static PropertyDefinition NuGetPackageId = PropertyDefinition.builder(PluginRulesDefinition.KEY + ".nuget.packageId")
+  public static PropertyDefinition NuGetPackageId = PropertyDefinition.builder(PluginRulesDefinition.REPOSITORY_ID + ".nuget.packageId")
 	.defaultValue("[ROSLYN_NUGET_PACKAGE_ID]")
 	.hidden()
 	.build();
 
-  public static PropertyDefinition NuGetPackageVersion = PropertyDefinition.builder(PluginRulesDefinition.KEY + ".nuget.packageVersion")
+  public static PropertyDefinition NuGetPackageVersion = PropertyDefinition.builder(PluginRulesDefinition.REPOSITORY_ID + ".nuget.packageVersion")
 	.defaultValue("[ROSLYN_NUGET_PACKAGE_VERSION]")
 	.hidden()
 	.build();
