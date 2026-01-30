3edf17043f8c9f94560a5d97eab8da6fe794c112
Alex Meseldzija <alexander.meseldzija@sonarsource.com>
2026-01-30 12:15:04 +0100

    I aint dealing with fixing copy paste

--- PluginGenerator/SonarQube/Serializer.cs
+++ PluginGenerator/SonarQube/Serializer.cs
@@ -35,7 +35,7 @@ namespace SonarQube.Common
         /// <summary>
         /// Save the object as XML
         /// </summary>
-        public static void SaveModel<T>(T model, string fileName) where T : class
+        public static void SaveModel<T>(T model, string fileName)
         {
             if (model == null)
             {
